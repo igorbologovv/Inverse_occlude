b3add54f35a54ce1599bb467b571e3fcb409ad63
Igor Bologov <peteshkus@gmail.com>
2023-05-05 18:04:36 +0300

    Complete occluding logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,64 +1,102 @@
+use std::f32::MIN;
 use box_intersect_ze::*;
+use box_intersect_ze::boxes::BBox;
 
 pub type BOX = boxes::Box2Df32;
 
-pub enum OcclusionStatus{
+pub enum OcclusionStatus {
     Occluded,
-    PartiallyVisible
+    PartiallyVisible,
+}
+#[derive(Clone)]
+pub struct OcclusionBuffer {
+    pub free_space: set::BBoxSet<BOX, usize>,
+    //this is silly but library wants this to store the box for inters check
+    new_box: set::BBoxSet<BOX, usize>,
+    box_idx_alloc: std::ops::RangeFrom<usize>,
+    occlusion_status: Vec<(usize, usize)>,
 }
 
-pub struct OcclusionBuffer{
-   pub free_space: set::BBoxSet<BOX, usize>,
-   //this is silly but library wants this to store the box for inters check
-   new_box: set::BBoxSet<BOX, usize>,
-   box_idx_alloc: std::ops::RangeFrom<usize>,
-   occlusion_status: Vec<(usize,usize)>,
-}
 
-impl OcclusionBuffer{
-pub fn new()->Self{
-    OcclusionBuffer { free_space: set::BBoxSet::new(), new_box: set::BBoxSet::new(),
-    box_idx_alloc: 1..,  occlusion_status:Vec::with_capacity(16)}
-}
-
-pub fn check_a_box(&mut self, new:BOX)->OcclusionStatus
-{
-    self.new_box.clear();
-    self.occlusion_status.clear();
-    self.new_box.push(usize::MAX - 1, new);
-    box_intersect_ze::intersect_scan(self.free_space, &self.new_box, &mut self.occlusion_status);
-
-    if self.occlusion_status.is_empty(){
-        OcclusionStatus::Occluded
+impl OcclusionBuffer {
+    pub fn new() -> Self {
+        OcclusionBuffer {
+            free_space: set::BBoxSet::new(),
+            new_box: set::BBoxSet::new(),
+            box_idx_alloc: 1..,
+            occlusion_status: Vec::with_capacity(16),
+        }
     }
-    else {
-        OcclusionStatus::PartiallyVisible
+
+
+    pub fn check_a_box(&mut self, new: BOX) -> OcclusionStatus {
+        self.new_box.clear();
+        self.occlusion_status.clear();
+        self.new_box.push(usize::MAX - 1, new);
+        box_intersect_ze::intersect_scan(
+            &self.free_space,
+            &self.new_box,
+            &mut self.occlusion_status,
+        );
+
+        if self.occlusion_status.is_empty() {
+            OcclusionStatus::Occluded
+        } else {
+            OcclusionStatus::PartiallyVisible
+        }
     }
-}
+    /// Adds box that was last passed into check_a_box
+    pub fn add_last_box(&mut self) {
+        //take stuff from self.new_box
+        assert!(!self.occlusion_status.is_empty());
+        // break up free space to accommodate new box
+    }
 
-fn add_freespace_box(&mut self, b:BOX){
+    fn add_freespace_box(&mut self, b: BOX) {
+        self.free_space.push(self.box_idx_alloc.next().unwrap(), b)
+    }
 
-    self.free_space.push(self.box_idx_alloc.next().unwrap(), b)
-}
-
-/// Adds box that was last passed into check_a_box
-pub fn add_last_box(&mut self){
-    //take stuff from self.new_box
-}
 
 }
 /** Checks if new box intersects free space.
 Returns vec of indices of free space regions intersected.
  */
-fn occlusion_check(free_space: &set::BBoxSet<BOX, usize>, new: BOX) -> Vec<usize> {
-    let mut newset = set::BBoxSet::new();
-    newset.push(usize::MAX - 1, new);
-    let mut result = Vec::new();
+fn intersection_check(free_space: &set::BBoxSet<BOX, usize>, new: BOX) -> Vec<usize> {
+    // create set for comparing two sets intersection
+    let mut new_set = set::BBoxSet::new();
+    new_set.push(usize::MAX - 1, new);
 
-    box_intersect_ze::intersect_scan(free_space, &newset, &mut result);
+    //create set which will collect the indices of the boxes from the old
+    //set of boxes, which were intersected
+    let mut result = Vec::new();
+    //find intersections and put it to the result vector
+    intersect_scan(free_space, &new_set, &mut result);
+    // Collect only indices from the old set
     result.iter().map(|e| e.0).collect()
 }
 
+fn covered_area_limits(free_space: &set::BBoxSet<BOX, usize>) -> ((f32, f32), (f32, f32)){
+
+    let mut  max =(f32::MIN, f32::MIN);
+    let mut  min =(f32::MAX, f32::MAX);
+
+    for i in &free_space.boxes{
+        if i.0.lo(0) < min.0 {
+            min.0 = i.0.lo(0)
+        }
+        if i.0.lo(1) < min.1 {
+            min.1 = i.0.lo(1)
+        }
+        if i.0.hi(0) > max.0{
+            max.0 = i.0.hi(0)
+        }
+        if i.0.hi(1) > max.1{
+            max.1 = i.0.hi(1)
+        }
+    }
+
+    (min, max)
+}
 /** Given vector of free space boxes and vec of indices of free space regions intersected by box new,
 breaks up boxes in free space until everything is correct again.
 Returns number of new boxes added to free space set
@@ -69,16 +107,125 @@ fn cut_space(
     new: BOX,
     start_idx: &mut std::ops::RangeFrom<usize>,
 ) {
+    // let mut collection_of_occluded_boxes = vec![];
+    // for i in intersected.boxes {
+    //     collection_of_occluded_boxes.push(free_space.boxes.get_mut(i).unwrap());
+    // }
+    // let guaranteed_free_space = covered_area_limits(&collection_of_occluded_boxes);
+    let mut tokill=vec![];
+    for i in intersected {
 
-    for i in intersected{
+        let (free, _fsp_index) = free_space.boxes.get_mut(i).unwrap();
 
+        let free_min = (free.lo(0), free.lo(1));
+        let free_max = (free.hi(0), free.hi(1));
+
+        let new_min = (new.lo(0), new.lo(1));
+        let new_max = (new.hi(0), new.hi(1));
+
+        let inters_vert;
+
+        let new_verts_in = [
+            free.contains_in(0, new_min.0) && free.contains_in(1, new_min.1),//
+            free.contains_in(0, new_min.0) && free.contains_in(1, new_max.1),
+            free.contains_in(0, new_max.0) && free.contains_in(1, new_max.1),
+            free.contains_in(0, new_max.0) && free.contains_in(1, new_min.1)
+        ];
+        inters_vert = new_verts_in.iter().map(|&e| e as usize).sum();
+       /// instead od deleting  boxes from free space  we mark them "empty"
+       /// and keep them in case we wold need to create more boxes when subdividing free space
+        match inters_vert {
+            0 =>{
+                // kill b because b is inside filled space
+                *free = BOX::new([f32::MAX,f32::MAX], [f32::MAX, f32::MAX]);
+            }
+            1 =>{
+                /// One vertex intersection
+                let rotation = new_verts_in.iter().position(|&e| e).unwrap();
+                match rotation {
+                    0=>{
+                        // Left lower corner intersection
+                        //todo the index is yet hardcoded make it unique
+                        //left upper corner
+                        *free = BOX::new([new.lo(0), free_max.1], [free_max.0, new.hi(1)]);
+
+                        //right upper corner
+                        free_space.push(start_idx.next().unwrap(), BOX::new([free_max.0, free_max.1],
+                                                         [new.hi(0), new.hi(1)]));
+                        //right lower corner
+                        free_space.push(start_idx.next().unwrap(), BOX::new([free_max.0,free_min.1],
+                                                    [new.hi(0), free_max.1]));
+
+                        }
+                    1=>{
+                        // Left upper corner intersection
+
+                        // left lower corner
+                        *free = BOX::new([new.lo(0),new.lo(1)], [free.hi(0), free.lo(1)]);
+
+                        // right upper corner
+                        free_space.push(start_idx.next().unwrap(),BOX::new([free.hi(0), free.lo(1)],
+                                                   [new.hi(0), new.hi(1)]));
+                        // right lower corner
+                        free_space.push(start_idx.next().unwrap(), BOX::new([free.hi(0),new.lo(1)],
+                                                    [new.hi(0), free.lo(1)]));
+
+                    }
+                    2=>{
+                        //Right upper corner intersection
+
+                        // Left lower corner box
+                        *free = BOX::new([new.lo(0),new.lo(1)], [free.lo(0), free.lo(1)]);
+
+                        //Left upper  corner box
+                        free_space.push(start_idx.next().unwrap(),BOX::new([new.lo(0), free.lo(1)], [free.lo(0), new.hi(1)]));
+                        // Right lower corner box
+                        free_space.push(start_idx.next().unwrap(),BOX::new([free.lo(0),new.lo(1)], [new.lo(0), free.lo(1)]));
+
+                    }
+                    3=>{
+                        //Right lower corner
+                        // Left lower corner
+                        *free =  BOX::new([new.lo(0),new.lo(1)], [free.lo(0), free.hi(1)]);
+
+                        // Left upper corner box
+                        free_space.push(start_idx.next().unwrap(),BOX::new([new.lo(0), free.hi(1)], [free.lo(0), new.hi(1)]));
+                        // Right upper corner
+                        free_space.push(start_idx.next().unwrap(),BOX::new([free.lo(0), free.hi(1)], [new.hi(0), new.hi(1)]));
+
+
+                    }
+                    _ => {}
+                }
+            }
+            2 =>{
+                // Two vertex intersection
+
+
+            }
+            3 => {
+                unreachable!()
+            }
+            4 => {
+
+            }
+            _=>{unreachable!()}
+        }
+    }
+
+        //amount of vert intersected free space
+
+    for i in tokill{
+        free_space.boxes.remove(i);
     }
     // start_idx.next();
     // free_space.boxes.remove(0);
     free_space.sort();
 }
 
-fn plot_intersections() {}
+
+
+
 
 #[cfg(test)]
 mod tests {
@@ -86,84 +233,30 @@ mod tests {
     use box_intersect_ze::boxes::BBox;
     use box_intersect_ze::*;
     use plotters::prelude::*;
+    use plotters::prelude::full_palette::GREEN_50;
+    use plotters::style::full_palette::BLUE_50;
+    use stdext::function_name;
 
-    const MAX_PIX: i32 = 256;
-    fn project_coords(b: BOX) -> ((i32, i32), (i32, i32)) {
-        let res = (
-            ((b.lo(0) * 100.) as i32 + 100, (b.lo(1) * 100.) as i32 + 100),
-            ((b.hi(0) * 100.) as i32 + 100, (b.hi(1) * 100.) as i32 + 100),
-        );
-        assert!(res.0 .0 > 0);
-        assert!(res.0 .1 < MAX_PIX);
-        assert!(res.1 .0 > 0);
-        assert!(res.1 .1 < MAX_PIX);
-        res
-    }
-    /* #[test]
-    fn bla() -> Result<(), Box<dyn std::error::Error>> {
-        let root = BitMapBackend::new("plotters-doc-data.png", (640, 480)).into_drawing_area();
-        root.fill(&WHITE)?;
-        let mut chart = ChartBuilder::on(&root)
-            .caption("y=x^2", ("sans-serif", 50).into_font())
-            .margin(5)
-            .x_label_area_size(30)
-            .y_label_area_size(30)
-            .build_cartesian_2d(-1f32..1f32, -0.1f32..1f32)?;
-
-        chart.configure_mesh().draw()?;
-
-        chart
-            .draw_series(LineSeries::new(
-                (-50..=50).map(|x| x as f32 / 50.0).map(|x| (x, x * x)),
-                &RED,
-            ))?
-            .label("y = x^2")
-            .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
-
-        let r = Rectangle::new([(0., 0.),(0.5,0.5) ], &RED);
-        let r2 = Rectangle::new([(0., 0.1),(0.5,0.5) ], &RED);
-        let t = Text::new("Kill me please", (0., 0.), ("sans-serif", 10).into_font());
-        chart.draw_series([r, r2]);
-        chart.draw_series([t]);
-        chart
-            .configure_series_labels()
-            .background_style(&WHITE.mix(0.8))
-            .border_style(&BLACK)
-            .draw()?;
-
-        root.present()?;
-
-        Ok(())
-    }*/
-
-    fn plotboxes(free_space: &set::BBoxSet<BOX, usize>, new: BOX, name: &str) {
-        let mut backend = SVGBackend::new(name, (MAX_PIX as u32, MAX_PIX as u32));
-        let style = {
-            let F = SVGBackend::new("/dev/null", (150, 150)).into_drawing_area();
-            ("sans-serif", 15.0, &BLUE).into_text_style(&F)
-        };
-
-        for (b, i) in &free_space.boxes {
-            let (lo, hi) = project_coords(*b);
-            backend.draw_rect(lo, hi, &BLUE, false).unwrap();
-            backend
-                .draw_text(&*i.to_string(), &style, lo)
-                .expect("TODO: panic message")
-        }
-        let (lo, hi) = project_coords(new);
-        backend.draw_rect(lo, hi, &RED, false).unwrap();
-        backend.present().unwrap();
-    }
-
+    ///
+    /// * `free`
+    /// * `new`
     fn test_inner(free: BOX, new: BOX, name: String, num_inters: usize) {
         let mut index_alloc = 1..;
         let mut free_space = set::BBoxSet::<BOX, usize>::new();
         free_space.push(index_alloc.next().unwrap(), free);
         plotboxes(&free_space, new, &(name.clone() + "__before.svg"));
 
-        let inters = occlusion_check(&free_space, new);
+        let inters = intersection_check(&free_space, new);
         assert_eq!(inters.len(), num_inters);
+        if num_inters == 0{return;}
         cut_space(&mut free_space, inters, new, &mut index_alloc);
+        {
+            let mut res = vec![];
+            intersect_scan(&free_space, &free_space, &mut res);
+            assert_eq!(res.len(), 0);
+            let inters = intersection_check(&free_space, new);
+            assert_eq!(inters.len(), 0);
+        }
         plotboxes(&free_space, new, &(name + "_after.svg"));
     }
 
@@ -236,9 +329,7 @@ mod tests {
         }
     }
 
-    fn better_name(s:&str)->String{
-        s.to_string().split("::").last().unwrap().to_string()
-    }
+
 
     #[test]
     pub fn height_width_overlap() {
@@ -264,6 +355,46 @@ mod tests {
     pub fn free_in_new() {
         let free = BOX::new([0., 0.], [1., 1.]); // base
         let new = BOX::new([-0.3, -0.3], [1.3, 1.3]);
-        test_inner(free, new, format!("hole/{} {}", &better_name(function_name!()), 1), 1);
+        test_inner(
+            free,
+            new,
+            format!("hole/{} {}", &better_name(function_name!()), 1),
+            1,
+        );
+    }
+
+    fn plotboxes(free_space: &set::BBoxSet<BOX, usize>, new: BOX, name: &str) {
+        let mut backend = SVGBackend::new(name, (MAX_PIX as u32, MAX_PIX as u32));
+        let style = {
+            let F = SVGBackend::new("/dev/null", (150, 150)).into_drawing_area();
+            ("sans-serif", 15.0, &BLUE).into_text_style(&F)
+        };
+
+        for (b, i) in &free_space.boxes {
+            let (lo, hi) = project_coords(*b);
+            backend.draw_rect(lo, hi, &BLUE_50, true).unwrap();
+            backend.draw_rect(lo, hi, &BLUE, false).unwrap();
+            backend
+                .draw_text(&*i.to_string(), &style, lo)
+                .expect("TODO: panic message")
+        }
+        let (lo, hi) = project_coords(new);
+        backend.draw_rect(lo, hi, &RED, false).unwrap();
+        backend.present().unwrap();
+    }
+    fn better_name(s: &str) -> String {
+        s.to_string().split("::").last().unwrap().to_string()
+    }
+     const MAX_PIX: i32 = 256;
+    fn project_coords(b: BOX) -> ((i32, i32), (i32, i32)) {
+        let res = (
+            ((b.lo(0) * 100.) as i32 + 100, (b.lo(1) * 100.) as i32 + 100),
+            ((b.hi(0) * 100.) as i32 + 100, (b.hi(1) * 100.) as i32 + 100),
+        );
+        assert!(res.0 .0 > 0);
+        assert!(res.0 .1 < MAX_PIX);
+        assert!(res.1 .0 > 0);
+        assert!(res.1 .1 < MAX_PIX);
+        res
     }
 }
